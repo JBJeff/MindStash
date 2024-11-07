6f5dd6e22d35300b980ed6ce80b3d44b0bae89aa
JBjeff <boettcher-jeffrey@t-online.de>
2024-11-07 16:28:58 +0100

    leerer NoteController

---
+++ RestApi/src/main/java/com/restApi/RestApi/Basics/controller/NoteController.java
@@ -0,0 +1,14 @@
+package com.restApi.RestApi.Basics.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+
+
+
+@RestController
+@RequestMapping("/notes")
+public class NoteController {
+
+    
+    
+}
