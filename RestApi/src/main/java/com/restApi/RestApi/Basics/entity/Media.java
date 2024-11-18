dc8772b07ed91a28b4271335337877c0823bbdd4
JBjeff <boettcher-jeffrey@t-online.de>
2024-11-18 20:48:27 +0100

    Media Entität überarbeitet

--- RestApi/src/main/java/com/restApi/RestApi/Basics/entity/Media.java
+++ RestApi/src/main/java/com/restApi/RestApi/Basics/entity/Media.java
@@ -6,12 +6,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "media")
 public class Media {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -20,11 +22,29 @@ public class Media {
     @JoinColumn(name = "note_id", nullable = false)
     private Note note; // Referenz zur Notiz
 
-    @Column(name = "url", nullable = false)
-    private String url; // URL für das Bild oder Link
+    @Column(name = "url")
+    private String url; // Optional: Für externe Links oder Dateien, die im Dateisystem gespeichert sind
 
     @Column(name = "type", nullable = false)
-    private String type; // Typ des Mediums (Bild, Link, etc.)
+    private String type; // Typ des Mediums (z. B. "image/png", "image/jpeg")
+
+    @Lob
+    @Column(name = "data")
+    private byte[] data; // Binäre Bilddaten
 
     // Getter und Setter
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public Note getNote() { return note; }
+    public void setNote(Note note) { this.note = note; }
+
+    public String getUrl() { return url; }
+    public void setUrl(String url) { this.url = url; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public byte[] getData() { return data; }
+    public void setData(byte[] data) { this.data = data; }
 }
