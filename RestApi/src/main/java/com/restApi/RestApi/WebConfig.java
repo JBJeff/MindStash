a25da4a315b7b00a4f13c153ee78b3ef5d9a7330
JBjeff <boettcher-jeffrey@t-online.de>
2024-11-19 10:48:14 +0100

    WebConfig für zugriff überarbeitet

--- RestApi/src/main/java/com/restApi/RestApi/WebConfig.java
+++ RestApi/src/main/java/com/restApi/RestApi/WebConfig.java
@@ -6,8 +6,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173"); // Vite-Frontend
+        // Erlaubt CORS für alle Anfragen, die auf das Backend zugreifen wollen
+        registry.addMapping("/**")  // Alle Endpunkte erlauben
+                .allowedOrigins("http://localhost:5175") // Nur vom Frontend (React) erlaubt
+                .allowedMethods("GET", "POST", "PUT", "DELETE") //  HTTP-Methoden 
+                .allowedHeaders("*") // Alle Header erlauben
+                .allowCredentials(true); // Cookies oder Header wie Authorization
     }
 }
